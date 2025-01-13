2a97844d3974a6bf3ec61be09b6a86e97df1fee6
jeremy collyer <collyerdesign@gmail.com>
2025-01-12 16:25:15 -0800

    move to edge function

---
+++ pages/api/chat-stream.ts
@@ -0,0 +1,30 @@
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req:Request, context: any) {
+  const { prompt } = await req.json();
+
+  const completion = await fetch(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        model: "gpt-4o-mini",
+        messages: [{ role: "user", content: prompt }],
+        stream: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPEN_AI_KEY}`,
+      },
+    }
+  );
+
+  return new Response(completion.body, {
+    status: 200,
+    headers: {
+      "content-type": "application/json: charset=utf-8",
+    },
+  });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,60 @@
-import { useEffect, useState, FormEvent } from 'react';
+import { useState, FormEvent } from 'react';
+import { createParser, type EventSourceMessage } from 'eventsource-parser';
 
 function HomePage() {
-  const [chat, setChat] = useState<string>('');
-  const [response, setResponse] = useState<string>('');
+  const [prompt, setPrompt] = useState<string>('');
+  const [output, setOutput] = useState<string>('');
 
-  const postChat = (e: FormEvent<HTMLFormElement>) => {
+  const postChat = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetch('/api/chat', {
+    // Clear the output
+    setOutput('');
+
+    const response = await fetch('/api/chat-stream', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ content: chat }),
-    })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      setResponse(data.choices[0].message.content);
+      body: JSON.stringify({ prompt }),
     });
+
+    const reader = response.body?.getReader();
+    const decoder = new TextDecoder();
+    const onEvent = (event: EventSourceMessage) => {
+      if (event) {
+        try {
+          const data = JSON.parse(event.data);
+          data.choices
+            .filter(({delta}: {delta: {content:any}}) => !!delta.content)
+            .forEach(({delta}: {delta: {content: any}}) => {
+              setOutput((prev) => `${prev}${delta.content}`);
+            });
+              
+        } catch (e) {
+          console.error('Error parsing JSON', e);
+        }
+      }
+    }
+
+    const parser = createParser({onEvent});
+
+    while (true) {
+      if (!reader) break;
+      const { done, value } = await reader.read();
+      const dataString = decoder.decode(value);
+      if (done || dataString.includes("[DONE]")) break;
+      parser.feed(dataString);
+    }
   }
 
   return (
     <div>
       <h1>Hello, World!</h1>
       <form onSubmit={(e) => postChat(e)}>
-        <input type="text" value={chat} onChange={(e) => setChat(e.target.value)} />
+        <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
         <button>submit</button>
       </form>
-      <div>{response}</div>
+      <div>{output}</div>
     </div>
   );
 }
